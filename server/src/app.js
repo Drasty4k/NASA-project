ef50c0ac163662065fe842503b84182a7ae3e5d6
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-07-31 15:20:36 +0300

    made the getAllPlanets endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const planetsRuter = require("./routes/planets/planets.router");
+
 const app = express();
+
 app.use(express.json());
+app.use(planetsRuter);
 
 module.exports = app;
