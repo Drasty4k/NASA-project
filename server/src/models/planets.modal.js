d76ee251b276148d530b6fb7ac9cb9218558608d
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-07-31 16:47:16 +0300

    Returned the planets data as a promise to be able to wait after the full data before the server starts

--- server/src/models/planets.modal.js
+++ server/src/models/planets.modal.js
@@ -1,7 +1,33 @@
-const { parse } = require("csv-parse");
 const fs = require("fs");
+const path = require("path");
+const { parse } = require("csv-parse");
 
 const habitablePlanets = [];
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, "..", "..", "data", "kepler_data.csv"))
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+      });
+  });
+}
 
 const isHabitablePlanet = (planet) => {
   return (
@@ -12,30 +38,7 @@ const isHabitablePlanet = (planet) => {
   );
 };
 
-fs.createReadStream("kepler_data.csv")
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on("error", (err) => {
-    console.log(err);
-  })
-  .on("end", () => {
-    console.log(
-      habitablePlanets.map((planet) => {
-        return planet["kepler_name"];
-      })
-    );
-    console.log(`${habitablePlanets.length} habitable planets found!`);
-  });
-
 module.exports = {
+  loadPlanetsData,
   planets: habitablePlanets,
 };
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,4 +1,4 @@
-const planets = require("../../models/planets.modal");
+const { planets } = require("../../models/planets.modal");
 
 function getAllPlanets(req, res) {
   return res.status(200).json(planets);
