42eeed144466c46fbd59679819d50a112271cde0
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-07-31 15:34:34 +0300

    installed and used the corse package

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const planetsRuter = require("./routes/planets/planets.router");
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:3001"
+}));
 app.use(express.json());
 app.use(planetsRuter);
 
