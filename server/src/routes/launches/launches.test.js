ff1b87a11120f48d99484a7d041405edcfddc456
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-08-01 15:55:52 +0300

    made 2 tests for some error cases in the POST "/launches" request

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -24,6 +24,13 @@ describe("Test POST /launches", () => {
     destination: "Test Planet",
   };
 
+  const launchDataWithInvalidDate = {
+    mission: "Mission Name Test",
+    rocket: "Test Rocket",
+    destination: "Test Planet",
+    launchDate: "not a date",
+  };
+
   test("It should respond with 201 created", async () => {
     const response = await request(app)
       .post("/launches")
@@ -38,7 +45,27 @@ describe("Test POST /launches", () => {
     expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 
-  test("It should catch missing required properties", () => {});
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
 
-  test("It should catch invalid dates", () => {});
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+  });
+
+  test("It should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
+    });
+  });
 });
